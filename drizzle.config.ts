a857f30e719acef2165f1e25646099b348056c19
Dev iLeaf <dev@ileafsolutions.com>
2025-01-06 18:24:38 +0530

    configured drizzle

---
+++ drizzle.config.ts
@@ -0,0 +1,10 @@
+import 'dotenv/config';
+import { defineConfig } from 'drizzle-kit';
+export default defineConfig({
+  out: './db/drizzle',
+  schema: './src/db/schema/index.ts',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+});
