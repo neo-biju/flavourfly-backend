9b6f1a59c8aea52c5a75b4dbacd3ac91741ac9b2
Dev iLeaf <dev@ileafsolutions.com>
2025-01-07 15:19:26 +0530

    added rate limiter

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,22 @@
 import "module-alias/register";
 import express from "express";
 import { errorHandler } from "@/middleware/error-handler";
+import { rateLimit } from "express-rate-limit";
+
 import env from "@/config/env";
 import routes from "@/routes";
 
 const app = express();
 const baseURL = "/api/v1";
+
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000,
+    limit: 100,
+    message:
+      "Too many requests from this IP, please try again after 15 minutes",
+  })
+);
 
 app.use(express.json());
 
